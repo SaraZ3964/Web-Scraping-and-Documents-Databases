3cdd44a412b9caa381eed20fcc4806ddcef8d9f5
Sirui Zhang <44712271+SaraZ3964@users.noreply.github.com>
2019-04-24 22:10:52 -0700

    Update Mars.py

--- Mars.py
+++ Mars.py
@@ -1,31 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 from bs4 import BeautifulSoup
 from splinter import Browser
 executable_path = {"executable_path": "\Users\saraz"}
 browser = Browser("chrome", **executable_path, headless=False)
 
-
-# In[7]:
-
-
 url = "https://mars.nasa.gov/news/"
 browser.visit(url)
 
-
-# In[8]:
-
-
 html = browser.html
 soup = BeautifulSoup(html, 'html.parser')
-
-
-# In[9]:
-
 
 article = soup.find("div", class_="list_text")
 news_p = article.find("div", class_="article_teaser_body").text
@@ -35,16 +17,8 @@ print(news_date)
 print(news_title)
 print(news_p)
 
-
-# In[10]:
-
-
 url2 = "https://jpl.nasa.gov/spaceimages/?search=&category=Mars"
 browser.visit(url2)
-
-
-# In[11]:
-
 
 html = browser.html
 soup = BeautifulSoup(html, 'html.parser')
@@ -52,20 +26,11 @@ image = soup.find("img", class_="thumb")["src"]
 img_url = "https://jpl.nasa.gov"+image
 featured_image_url = img_url
 
-
-# In[12]:
-
-
 import requests
 import shutil
 response = requests.get(img_url, stream=True)
 with open('img.jpg', 'wb') as out_file:
     shutil.copyfileobj(response.raw, out_file)
 
-
-# In[13]:
-
-
 from IPython.display import Image
 Image(url='img.jpg')
-
